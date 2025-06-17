90d763af8087ab85757acc24720d9b0826912434
dannyarena <arenadanny71@gmail.com>
2025-06-17 16:55:10 +0200

    add axios to get movie list

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,15 @@ import { useEffect, useState } from "react";
 
 export default function HomePage() {
     const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        axios.get("http://localhost:3000/movies")
+        .then(res => {
+            setMovies(res.data);
+        })
+        .catch(err => {
+            console.error("Errore nella chiamata API:", err);
+        });
+    }, []);
     return (
         <div>
             <h2>Home Page</h2>
