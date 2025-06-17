03059722d65efe43a899921083ddbcb801f79d42
dannyarena <arenadanny71@gmail.com>
2025-06-17 16:46:35 +0200

    create setmovies state

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
+
 export default function HomePage() {
+    const [movies, setMovies] = useState([]);
     return (
         <div>
             <h2>Home Page</h2>
