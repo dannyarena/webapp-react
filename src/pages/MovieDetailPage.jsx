2616c5f15f1ee9f51c2658c1d7297f86fc9cda3f
dannyarena <arenadanny71@gmail.com>
2025-06-17 17:20:52 +0200

    ad axios call to get movies from id

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,6 +1,20 @@
+const { id } = useParams();
+
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function MovieDetailPage() {
+    const [movie, setMovies] = useState(null);
+    useEffect(() => {
+        axios.get(`http://localhost:3000/movies/${id}`)
+        .then(res => {
+            setMovies(res.data);
+        })
+        .catch(err => {
+            console-error("Errore nella chimata", err);
+        });
+    }, [id]);
+    
     const { id } = useParams();
 
     return (
