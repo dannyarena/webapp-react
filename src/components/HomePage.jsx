cf2f1d9605bb6249231864256d7ad76b6b218cf4
dannyarena <arenadanny71@gmail.com>
2025-06-21 21:02:53 +0200

    import and use loader in homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,17 +2,26 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
+import { useLoader } from "../context/LoaderContext";
 
 export default function HomePage() {
     const [movies, setMovies] = useState([]);
+    const { setIsLoading } = useLoader();
+
     useEffect(() => {
+        setIsLoading(true);
+
         axios.get("http://localhost:3000/movies")
         .then(res => {
             setMovies(res.data);
         })
         .catch(err => {
             console.error("Errore nella chiamata API:", err);
+        })
+        .finally(() => {
+            setIsLoading(false);
         });
+
     }, []);
     return (
         <div>
