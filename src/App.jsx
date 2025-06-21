fc340185d9a7039948d6dcf341e227ac499ac851
dannyarena <arenadanny71@gmail.com>
2025-06-21 20:55:44 +0200

    add loading indicator to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,27 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import MovieDetailPage from './components/MovieDetailPage';
+import Loader from './components/Loader';
+import { useLoader } from './context/LoaderContext';
 
 function App() {
+
+  const { isLoading } = useLoader();
   return (
+    <>
     <div className='container'>
       <h1>WebApp React</h1>
+
+      {isLoading && <Loader />}
+
+
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies/:id" element={<MovieDetailPage />} />
       </Routes>
     </div>
+  </>
   )
-   
 }
 export default App
